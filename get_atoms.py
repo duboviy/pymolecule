967c4633eb6b946b70294f7d4b1f402b1a4dfb47
duboviy <eugene.dubovoy@gmail.com>
2017-05-02 00:23:30 +0300

    Update get_atoms.py

--- get_atoms.py
+++ get_atoms.py
@@ -6,8 +6,11 @@ def get_atoms(smile):
     molecule.make3D()
     return molecule.atoms
 
+
 if __name__=="__main__":
     # SMILE - simplified molecular-input line-entry system
     smile = input("Enter molecule in SMILE format (e.g., Glucose -> OC[C@H]1OC(O)[C@H](O)[C@@H](O)[C@@H]1O:")
-    print(get_atoms(smile))
+    atoms = get_atoms(smile)
     
+    for atom in atoms:
+        print(atom.type, ' '.join(map(str, atom.coords)))
