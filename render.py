5e02c8498070546483c848625de86ff158ce174c
duboviy <eugene.dubovoy@gmail.com>
2017-07-09 23:18:03 +0300

    Update render.py

--- render.py
+++ render.py
@@ -16,14 +16,24 @@ def on_draw():
 
 @window.event
 def on_resize(width, height):
-    glClearColor(0.0, 0.3, 0.0, 0.0)
+    glClearColor(0.0, 0.3, 0.0, 0.0) # set the background color
     
-    glEnable(GL_DEPTH_TEST)
+    glEnable(GL_DEPTH_TEST) # enable depth buffer
     glEnable(GL_LIGHTING)
     glEnable(GL_LIGHT0)
-    glLightf(GL_LIGHT0, GL_POSITION, 1, 5, 4)
+    glLightf(GL_LIGHT0, GL_POSITION, 1, 5, 4) # put one light
     
-    pass # TODO: implement!
+    glViewport(0, 0, width, height)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(45, width / height, .1, 1000) # perspective projection angle 45 degrees
+    gluLookAt( # set the camera and aim it at the center of the scene
+     1, 4, 15, # eye
+     0, 0, 0, # target
+     0, 1, 0  # up
+    )
+    glMatrixMode(GL_MODELVIEW) 
+    return pyglet.event.EVENT_HANDLED
 
 @window.event
 def on_key_press(symbol, modifiers):
