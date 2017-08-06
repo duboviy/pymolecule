c5e8b83e66ca2dc643f75323c80ca2524e850054
duboviy <eugene.dubovoy@gmail.com>
2017-08-06 13:11:57 +0300

    Add molecule model represantaion

--- render.py
+++ render.py
@@ -10,6 +10,49 @@ from pyglet.gl.glu import *
 window = Window()
 
 
+class Molecule(object):
+    def __init__(self, atoms):
+        self.atoms = atoms
+ 
+    @classmethod
+    def import_from_file(self, fn):
+        """ An alternative constructor to instantiate molecule from text file. """
+        atoms = []
+        with open(fn) as f:
+            for l in f:
+                el, x, y, z = l.split()
+                self.atoms.append(
+                    (el, float(x), float(y), float(z))
+                )
+         return cls(atoms)
+ 
+    def draw(self):
+        for atom in self.atoms:
+            glPushMatrix() # save model matrix
+            glTranslatef(*atom[1:]) # shift the matrix of the model to the atom coordinates
+            # draw the sphere of radius 1 and the color of the corresponding atom type
+            draw_sphere(1, palette.get_color(atom[0]))
+            glPopMatrix() # load the saved matrix of the model
+ 
+ 
+molecule = Molecule.import_from_file('glucose.dat')
+ 
+ 
+def draw_sphere(radius, color):
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
+     
+    # set matrerial color
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, (GLfloat * 3)(*color))
+    glMaterialfv(GL_FRONT_AND_BACK, GL_EMISSION,
+        (GLfloat * 3)(*map(lambda x: x/2, color))
+    )
+    # glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, (GLfloat * 3)(*color))
+ 
+    sphere = gluNewQuadric()
+    gluSphere(sphere, radius, 50, 50) # 50, 50 - amount of meridians and parallels
+    # if you need more atoms - reduce their amount to increase performance
+    
+
 def update_frame(dt):
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity() # load identity matrix
@@ -17,7 +60,7 @@ def update_frame(dt):
     # TODO: implement rotation logic without global vars
     global rotation
     rotation += dt * 10 # more time - bigger rotation
-    glRotatef(rotation, 0, 1, 0)
+    glRotatef(rotation, 0, 1, 0) # around y axis
     molecule.draw()
 
     
